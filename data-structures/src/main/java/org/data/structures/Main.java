1879ab13421184e9fc96c3e4a489bb570cb7362d
Ian Silva Santos <ian.santos@sptech.school>
2024-08-11 09:46:08 -0300

    feat: Teste do filter.

--- data-structures/src/main/java/org/data/structures/Main.java
+++ data-structures/src/main/java/org/data/structures/Main.java
@@ -8,9 +8,17 @@ public class Main {
         for(int i = 0; i < 30; i++){
             customArrayList.add(i);
         }
+        System.out.println("---------------------------------------");
+        System.out.println("Teste do forEach");
         customArrayList.forEach(System.out::println);
+        System.out.println("---------------------------------------");
+        System.out.println("Teste do size e remove");
         System.out.println(customArrayList.size());
         customArrayList.remove(0);
         System.out.println(customArrayList.size());
+        System.out.println("---------------------------------------");
+        System.out.println("Teste do filter");
+        customArrayList.filter(valor -> valor > 15).forEach(System.out::println);
+        System.out.println("---------------------------------------");
     }
 }
