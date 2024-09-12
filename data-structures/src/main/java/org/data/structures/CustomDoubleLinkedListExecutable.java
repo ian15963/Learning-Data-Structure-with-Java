1a0422d8cde5c572c7fe8edf32e6a2344c9f95b1
Ian Silva Santos <ian.santos@sptech.school>
2024-09-12 11:12:57 -0300

    feat: Adicionando lógica dos métodos, insert, set e remove na doubledLinkedList.

--- data-structures/src/main/java/org/data/structures/CustomDoubleLinkedListExecutable.java
+++ data-structures/src/main/java/org/data/structures/CustomDoubleLinkedListExecutable.java
@@ -10,7 +10,12 @@ public class CustomDoubleLinkedListExecutable {
         CustomDoubleLinkedList<String> linkedList = new CustomDoubleLinkedList<>();
         linkedList.add("Olá, mundo!");
         linkedList.add("Batata");
-        System.out.println(linkedList.get(1));
+        linkedList.add("Mamão");
+//        linkedList.insert("Abacate", 1);
+//        linkedList.set("Abacaxi", 1);
+//        System.out.println(linkedList.get(2));
+        linkedList.remove("Batata");
+        System.out.println(linkedList);
     }
 
 }
--- data-structures/src/main/java/org/data/structures/listaencadeada/CustomDoubleLinkedList.java
+++ data-structures/src/main/java/org/data/structures/listaencadeada/CustomDoubleLinkedList.java
@@ -8,6 +8,7 @@ public class CustomDoubleLinkedList<T> {
     public void add(T value){
         if(first == null){
             first = new Node<>(value);
+            return;
         }
         Node<T> current = first;
         while(current.next != null){
@@ -18,17 +19,93 @@ public class CustomDoubleLinkedList<T> {
         current.next = newNode;
     }
 
+    public void set(T value, int indice){
+        if (first == null || indice < 0){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        Node<T> current = first;
+        for (int i = 0; i < indice; i++) {
+            if (current.next == null){
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            current = current.next;
+        }
+        Node<T> newNode = new Node<>(value);
+        if (current == first){
+            newNode.next = first.next;
+            first.next.previous = newNode;
+            first = newNode;
+            return;
+        }
+        if (current.next != null){
+            current.next.previous = newNode;
+        }
+        if (current.previous != null){
+            current.previous.next = newNode;
+        }
+        newNode.previous = current.previous;
+        newNode.next = current.next;
+    }
+
+    public void insert(T value, int indice){
+        if (first == null || indice < 0){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        Node<T> current = first;
+        for (int i = 0; i < indice; i++) {
+            if (current.next == null){
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            current = current.next;
+        }
+        Node<T> newNode = new Node<>(value);
+        if (current == first){
+            newNode.next = first;
+            first.previous = newNode;
+            first = newNode;
+            return;
+        }
+        current.previous.next = newNode;
+        newNode.previous = current.previous;
+        newNode.next = current;
+        current.previous = newNode;
+    }
+
+    public void remove(T value){
+        if (first == null){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        Node<T> current = first;
+        while (!current.getValue().equals(value)){
+            if (current.next == null){
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            current = current.next;
+        }
+        if (current == first){
+            first = current.next;
+            first.previous = null;
+            return;
+        }
+        current.previous.next = current.next;
+        if (current.next != null){
+            current.next.previous = current.previous;
+        }
+
+    }
+
     public T get(int indice){
         if(first == null || indice < 0){
             throw new ArrayIndexOutOfBoundsException();
         }
         Node<T> current = first;
-        for (int i = 0; i <= indice; i++){
+        for (int i = 0; i < indice; i++){
             if(current.next == null){
                 throw new ArrayIndexOutOfBoundsException();
             }
             current = current.next;
         }
+
         return current.getValue();
     }
 
